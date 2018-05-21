95151e4a699ec34f2678acf8acae6031d511c440
Riley Mathews <rileymathews80@gmail.com>
2018-05-21 15:44:58 -0500

    refactored using bulma

--- src/scripts/search/buildGameCard.js
+++ src/scripts/search/buildGameCard.js
@@ -10,14 +10,13 @@ const buildGameCard = (game) => {
     console.log(game)
     const card = $(
         `
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="${game.image.small_url}" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">${game.name}</h5>
-                    <p class="card-text">${game.deck}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <article class="media">
+                <img class="media-left" src="${game.image.thumb_url}" alt="Card image cap">
+                <div>
+                    <h2 class="title">${game.name}</h2>
+                    <p>${game.deck}</p>
                 </div>
-            </div>
+            </article>
         `
     )
     return card
