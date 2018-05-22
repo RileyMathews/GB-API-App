959a45d0cdcca8e350bc6de4ea86a4e47bd22343
Riley Mathews <rileymathews80@gmail.com>
2018-05-22 08:44:22 -0500

    modified styling

--- src/scripts/search/buildGameCard.js
+++ src/scripts/search/buildGameCard.js
@@ -13,7 +13,7 @@ const buildGameCard = (game) => {
             <article class="media">
                 <img class="media-left" src="${game.image.thumb_url}" alt="Card image cap">
                 <div>
-                    <h2 class="title">${game.name}</h2>
+                    <h2 class="title is-3">${game.name}</h2>
                     <p>${game.deck}</p>
                 </div>
             </article>
